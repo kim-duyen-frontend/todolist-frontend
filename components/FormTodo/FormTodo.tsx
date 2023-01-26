98d974e8f7dd4386cf53cd2cc5b902a66d142ee1
kim-duyen-frontend <ntduyen255@gmail.com>
2023-01-26 17:32:09 +0700

    feature: check form valid

--- components/FormTodo/FormTodo.tsx
+++ components/FormTodo/FormTodo.tsx
@@ -1,16 +1,44 @@
 import { useAppDispatch, useAppSelector } from '@/app/hooks';
-import { addToDoList, getListTodo, selectCollectionTodoceSelector, setTextForm } from '@/features';
+import { addToDoList, getListTodo, selectCollectionTodoceSelector, setMessageForm, setTextForm, TTodolist } from '@/features';
 import { Box, Button, Group, Textarea } from '@mantine/core';
-import React from 'react';
+import React, { useEffect } from 'react';
 
+const getValidForm = (input_form: string, listTodo: TTodolist[]) => {
+    for (let i = 0; i < listTodo.length; i++) {
+        const element = listTodo[i];
+        if (element.text === input_form) {
+            return "This text already exists"
+        }
+    }
+    if (input_form.length < 2 || input_form.length > 50) {
+        return "The length must be greater than or equal to 50"
+    } else {
+        for (let i = 0; i < listTodo.length; i++) {
+            const element = listTodo[i];
+            if (element.text === input_form) {
+                return "This text already exists"
+            }
+        }
+    }
+}
 const FormTodo = () => {
     const dispatch = useAppDispatch()
-    const { input_form } = useAppSelector(selectCollectionTodoceSelector)
-    
+    const { input_form, listTodo, message } = useAppSelector(selectCollectionTodoceSelector)
+
     const handleAddToDo = async () => {
-        await dispatch(addToDoList({ text: input_form, status: false }))
-        await dispatch(getListTodo())
+        let msg = getValidForm(input_form, listTodo)
+        if (msg) {
+            dispatch(setMessageForm(msg))
+        } else {
+            await dispatch(addToDoList({ text: input_form, status: false }))
+            await dispatch(getListTodo())
+        }
     }
+
+    useEffect(() => {
+        dispatch(setMessageForm(" "))
+    }, [input_form])
+
     return (
         <Box
             sx={(theme) => ({
@@ -26,7 +54,7 @@ const FormTodo = () => {
                 withAsterisk
                 onChange={(event) => dispatch(setTextForm(event.target.value))}
             />
-
+            {message ? <span className='text-red-500 font-bold'>{message}</span> : ""}
             <Group position="right">
                 <Button
                     className='hover: bg-transparent my-3'
